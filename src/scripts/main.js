1d181e4093c9661263269d91a5a37632ebb21612
sanghyun <stlblue@naver.com>
2017-07-15 19:57:25 +0900

    Edit mailchimp response message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,16 +5,9 @@ if ( $(window).width() < 836 ) {
 
 	$('.br_remove').hide();
 }
-
 else {
-
 }
-
-
-
 });
-
-
 
 $('#mc-embedded-subscribe-form').submit(function(e){
 	var $this = $(this);
@@ -27,23 +20,26 @@ $('#mc-embedded-subscribe-form').submit(function(e){
       error       : function(err) { alert("Could not connect to the registration server."); },
       success     : function(data) {
           if (data.result != "success") {
-          	$('#mce-error-response').html('');
-            $('mce-success-response').css('display','none');
-          	data.msg = '오류가 발생했습니다. 다시 시도해주세요.';
-          		console.log('error happend');
-          		$('#mce-error-response').html(data.msg);
-          		console.log(data.msg);
+            $('#mce-error-response').html('');
+            data.msg = '오류가 발생했습니다. 다시 시도해주세요.';
+            $('#mce-success-response').html('');
+            $('#mce-success-response').css('display','none');
+            $('#mce-error-response').css('display','block');
+          	console.log('error happend');
+          	$('#mce-error-response').html(data.msg);
+            console.log(data.msg);
 
           } else {
-          	$('#mce-success-response').html('');
-            $('mce-error-response').css('display','none');
-          	data.msg = '이메일이 발송되었습니다.';
+            $('#mce-success-response').html('');
+            data.msg = '이메일이 발송되었습니다.';
+          	$('#mce-error-response').html('');
+            $('#mce-error-response').css('display','none');
+            $('#mce-success-response').css('display','block');
           	$('#mce-success-response').html(data.msg);
           	console.log('success');
+            console.log(data.msg);
           }
       }
   });
   return false;
 });
-
-//제이쿼리 css묶어서 display block이랑 none조정하기
